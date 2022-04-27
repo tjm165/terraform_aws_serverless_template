a48e55a427ec12e457c9b0fa1feea343fbf70744
Thomas M <tjm165@case.edu>
2022-04-27 00:13:37 -0500

    lambda to read s3 directory

--- src/api_lambda_s3/lambda.py
+++ src/api_lambda_s3/lambda.py
@@ -1,5 +1,6 @@
 import json
 from decimal import Decimal
+import boto3
 
 def default(obj):
     if isinstance(obj, Decimal):
@@ -7,11 +8,21 @@ def default(obj):
     raise TypeError("Object of type '%s' is not JSON serializable" % type(obj).__name__)
 
 def lambda_handler(event, context):
-    resp = {
-        "message": "Hello S3 Tommy",
-    }
+    s3 = boto3.resource('s3')
+    bucket = s3.Bucket('serverlesscoffeeshops3')
+    
+    content = "<!DOCTYPE html><html><head><title>Serverless API Lambda S3</title></head><body><h1>Welcome to S3 from Lambda</h1><h2>The contents of this s3 bucket are</h2>"
+    
+    for obj in bucket.objects.all():
+        content += "<p> /" + obj.key + "</p>"
+        print(obj.key)
+        
+    content += "</body></html>"
 
     return {
         'statusCode': 200,
-        'body' : json.dumps(resp, default=default)
+        'body' : content,
+        "headers": {
+            'Content-Type': 'text/html',
+        }
     }
